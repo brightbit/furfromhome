a000202df433d5126326650ecfb0238248e02453
Eric Boehs <ericboehs@gmail.com>
2013-05-22 15:51:59 -0500

    Add has_scope with lots of scopes/filters

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,32 +1,31 @@
 class PetsController < ApplicationController
-  before_action :set_pet, only: [:show, :edit, :update, :destroy]
 
-  # GET /pets
-  # GET /pets.json
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+  has_scope :has_location, default: true, type: :boolean
+  has_scope :listing_type
+  has_scope :missing_since
+  has_scope :source
+  has_scope :breed
+  has_scope :animal
+  has_scope :color
+  has_scope :gender
+  has_scope :page, default: 1
+  has_scope :near
+
   def index
-    if params[:search].present?
-      @pets = Pet.near(params[:search], 15, order: 'distance').paginate(page: params[:page])
-    else
-      @pets = Pet.all.paginate(page: params[:page])
-    end
+    @pets = apply_scopes(Pet).all
   end
 
-  # GET /pets/1
-  # GET /pets/1.json
   def show
   end
 
-  # GET /pets/new
   def new
     @pet = Pet.new
   end
 
-  # GET /pets/1/edit
   def edit
   end
 
-  # POST /pets
-  # POST /pets.json
   def create
     @pet = Pet.new(pet_params)
 
@@ -41,8 +40,6 @@ class PetsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pets/1
-  # PATCH/PUT /pets/1.json
   def update
     respond_to do |format|
       if @pet.update(pet_params)
@@ -55,8 +52,6 @@ class PetsController < ApplicationController
     end
   end
 
-  # DELETE /pets/1
-  # DELETE /pets/1.json
   def destroy
     @pet.destroy
     respond_to do |format|
