466fb966c3272666c44326a3c394b0c7b987a2aa
Jonathan Camenisch <jonathan@camenisch.net>
2013-05-22 02:39:21 -0500

    Add cat breeds too

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,34 @@ require 'active_support'
 
 user = User.where(email: 'user@example.com').first || User.create!(email: 'user@example.com', password: "password", password_confirmation: 'password', admin: true)
 
-File.readlines(File.realpath("../seeds/breed_names.tsv", __FILE__)).each do |line|
-  names = line.chomp.split("\t").map do |n|
-    {
-      name: n,
-      simplified_name: ActiveSupport::Inflector.transliterate(n).downcase
-    }
-  end
+def import_breed_names(species)
+  File.readlines(File.realpath("../seeds/#{species}_breed_names.tsv", __FILE__)).each do |line|
+    names = line.chomp.split("\t").map do |n|
+      {
+        name: n,
+        simplified_name: ActiveSupport::Inflector.transliterate(n).downcase
+      }
+    end
 
-  breed_id = nil
+    breed_id = nil
 
-  names.each do |name|
-    BreedName.where(
-      simplified_name: name[:simplified_name]
-    ).first_or_create do |record|
+    names.each do |name|
+      BreedName.where(
+        simplified_name: name[:simplified_name]
+      ).first_or_create do |record|
+        record.name = name[:name]
 
-      record.name = name[:name]
-      name[:record] = record
-      record.breed_id = breed_id ||= Breed.create.id
+        name[:record] = record
 
+        if breed_id
+          record.breed_id = breed_id
+        else record.breed_id
+          breed_id = record.breed_id ||= Breed.create(species: species).id
+        end
+      end
     end
   end
 end
+
+import_breed_names('dog')
+import_breed_names('cat')
