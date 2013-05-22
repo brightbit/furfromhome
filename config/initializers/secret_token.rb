e8f4232af421caa7ed569f8673dea20f05df7f30
Eric Boehs <ericboehs@gmail.com>
2013-05-21 19:57:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Furfromhome::Application.config.secret_key_base = '3deda3b2ee422da20be2df0e5eee6ba7a232903e339fef3ce5dc7f3a48b57b11eedd86cad1e550915b8b67c2fe298e8fe6f0bdf742aaf5697ae1c56e582e8251'
